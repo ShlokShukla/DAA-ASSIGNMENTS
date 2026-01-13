94e012a3dd3b83e82d7c87e439f8327a9b17fd0d
ShlokShukla <83460492+ShlokShukla@users.noreply.github.com>
2026-01-13 18:20:28 +0530

    Update question1.cpp

--- assignment1/question1.cpp
+++ assignment1/question1.cpp
@@ -1,1 +1,27 @@
-
+#include <iostream>
+using namespace std;
+void binarySearch(int arr[] , int low , int high, int x)
+{
+    int mid=(low+high)/2;
+    if (low>high)
+    {
+        return;
+    }
+    if(x==arr[mid])
+    {
+        cout<<"Element found at "<< mid;
+    }
+    else if(x<arr[mid])
+    {
+        binarySearch(arr , low , mid , x);
+    }
+    else if(x>arr[mid])
+    {
+        binarySearch(arr , mid+1 , high , x);
+    }
+}
+int main()
+{
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    binarySearch(arr , 0 , 10 , 23);
+}
